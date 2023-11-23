cd1847168784b6859f98cc94a701aed90be76d64
shalanoveljan <eljanash@code.edu.az>
2023-11-23 23:49:38 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Wolt.Service.Services.Implementations;
+
+ProductService productService = new();
+
+
+Console.WriteLine("1.Create");
+Console.WriteLine("2.getAll");
+Console.WriteLine("3.Delete");
+Console.WriteLine("4.GetbyId");
+Console.WriteLine("5.GetAllFilter");
+Console.WriteLine("6.Update");
+Console.WriteLine("0.Close");
+
+Console.WriteLine("0.Close");
+string request = Console.ReadLine();
+
+while (request != "0")
+{
+    switch (request)
+    {
+        case "1":
+            productService.Create();
+            break;
+        case "2":
+            productService.GetAll();
+            break;
+        case "3":
+            productService.Delete();
+            break;
+        case "4":
+            productService.GetbyId();
+            break;
+            case "5":
+                productService.GetAllFilter();
+            break;
+        case "6":
+            productService.Update();
+            break;
+        default:
+            Console.WriteLine("Add valid option");
+            break;
+    }
+
+    Console.WriteLine("1.Create");
+    Console.WriteLine("2.getAll");
+    Console.WriteLine("3.Delete");
+    Console.WriteLine("4.GetbyId");
+    Console.WriteLine("5.GetAllFilter");
+    Console.WriteLine("6.Update");
+    Console.WriteLine("0.Close");
+    
+
+    request = Console.ReadLine();
+}
